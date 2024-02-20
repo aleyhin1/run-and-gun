8e204c113dceaf0901ff60d00dea43d76db109ba
Batu <aleyhin1@gmail.com>
2024-02-21 00:39:36 +0300

    enemy created

---
+++ Assets/_GameAssets/Code/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private Transform _playerTransform;
+
+    private void OnEnable()
+    {
+        _playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        Vector3 directionToMove = (_playerTransform.position - transform.position).normalized;
+        transform.Translate(directionToMove * _speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Bullet"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
